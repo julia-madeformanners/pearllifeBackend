a967237a7a6b5a137f3d2db783d1656a44b951f6
Meery Awad <meeryawad19@gmail.com>
2026-01-07 14:16:58 +0400

    email

--- index.js
+++ index.js
@@ -92,11 +92,11 @@ app.post("/payment-notification", async (req, res) => {
         console.log('✅ SMTP server is ready to send messages');
       }
     });
-   
+
     const mailOptions = {
-      from: `"Website Payment Notification`,
+      from: `"Website Payment Notification" <hello@pearllifefuneralservices.com>`,
       to: user.email,
-      cc:"hello@pearllifefuneralservices.com",
+      cc: "hello@pearllifefuneralservices.com",
       subject: `🧾 Payment Invoice - ${user.name}`,
       html: `
         <div style="font-family:Arial,sans-serif;padding:20px;background:#f4f4f4;">
